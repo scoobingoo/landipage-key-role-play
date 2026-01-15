7ca908287cb424380f8361d8ee60fc8c3c37cf5a
Sonic Lee <sonlocle36123@gmail.com>
2026-01-16 05:08:10 +0700

    hihihihi

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // THÊM DÒNG NÀY VÀO ĐẦU TIÊN
+      base: '/landipage-key-role-play/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
